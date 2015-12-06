ac2cea2a3253d7ac3f06d46fbb1dd2660de20e02
deiu <andrei@fcns.eu>
2015-12-06 00:22:11 -0500

    Added default text when no posts exist

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 /* ---- DON'T EDIT BELOW ---- */
 var editor = new SimpleMDE({
     status: false,
-    spellChecker: false
+    spellChecker: false,
+    initialValue: 'This is a markdown editor, type something...'
 });
 var parseMD = function(data) {
     if (data) {
@@ -22,55 +23,14 @@ var setBodyValue = function(val) {
 var user = {
         title: "/dev/solid",
         tagline: "Rocking the Solid Web",
-        picture: "https://deiu.me/avatar.jpg",
-        name: "Andrei Sambra",
-        webid: "https://deiu.me/profile#me",
-        avatar: "https://deiu.me/avatar.jpg"
+        picture: "img/logo-white.svg",
+        name: "John Doe",
+        webid: "https://example.org/user#me",
+        avatar: "img/icon.svg"
 };
 
-var init = function() {
-    document.querySelector('.blog-picture').src = user.picture;
-    document.querySelector('.blog-title').innerHTML = user.title;
-    document.querySelector('.blog-tagline').innerHTML = user.tagline;
-
-    if (queryVals['view'] && queryVals['view'].length > 0) {
-        var url = decodeURIComponent(queryVals['view']);
-        showViewer(url);
-    } else if (queryVals['edit'] && queryVals['edit'].length > 0) {
-        var url = decodeURIComponent(queryVals['edit']);
-        showEditor(url);
-    } else if (queryVals['new'] !== undefined) {
-        showEditor();
-    }
-    // select element holding all the posts
-    var postsdiv = document.querySelector('.posts');
-
-    // add all posts to viewer
-    for (var i in posts) {
-        var article = addPostToDom(posts[i]);
-        postsdiv.appendChild(article);
-    }
-
-    var header = document.querySelector('.header');
-    var header_height = getComputedStyle(header).height.split('px')[0];
-    var nav = document.querySelector('.nav');
-    var pic = document.querySelector('.blog-picture');
-    var pic_height = getComputedStyle(pic).height.split('px')[0];
-    var diff = header_height - pic_height;
-
-    function stickyScroll(e) {
-        if (window.pageYOffset > (diff + 50)) {
-            nav.classList.add('fixed-nav');
-        }
-
-        if(window.pageYOffset < (diff + 50)) {
-            nav.classList.remove('fixed-nav');
-        }
-    }
-
-    // Scroll handler to toggle classes.
-    window.addEventListener('scroll', stickyScroll, false);
-};
+var posts = {};
+var authors = {};
 
 var confirmDelete = function(url) {
     var postTitle = (posts[url].title)?'<br><p><strong>'+posts[url].title+'</strong></p>':'this post';
@@ -182,6 +142,7 @@ var showEditor = function(url) {
     document.querySelector('.nav').classList.add('hidden');
     document.querySelector('.posts').classList.add('hidden');
     document.querySelector('.viewer').classList.add('hidden');
+    document.querySelector('.start').classList.add('hidden');
     document.querySelector('.editor').classList.remove('hidden');
     document.querySelector('.editor-title').focus();
     document.querySelector('.editor-author').innerHTML = user.name;
@@ -250,9 +211,18 @@ var resetAll = function() {
     document.querySelector('.posts').classList.remove('hidden');
     document.querySelector('.editor-title').innerHTML = '';
     document.querySelector('.editor-author').innerHTML = '';
-    document.querySelector('.editor-date').innerHTML = '';
-    document.querySelector('.editor-body').innerHTML = '';
-    // window.location.search = '';
+    document.querySelector('.editor-date').innerHTML = moment().format('LL');
+    document.querySelector('.editor-tags').innerHTML = '';
+    document.querySelector('.editor-add-tag').value = '';
+    setBodyValue('');
+
+    console.log(posts);
+    if (posts && Object.keys(posts).length === 0) {
+        document.querySelector('.start').classList.remove('hidden');
+    } else {
+        document.querySelector('.start').classList.add('hidden');
+    }
+
     window.history.pushState("", "Plume", window.location.pathname);
 };
 
@@ -298,61 +268,61 @@ var publishPost = function(url) {
     resetAll();
 };
 
-var posts = {
-    "https://example.org/post1": {
-        url: "https://example.org/post1",
-        title: "Introducing Solid",
-        author: "https://deiu.me/profile#me",
-        date: "4 Dec 2015",
-        body: "![test](https://deiu.me/avatar.jpg) \n\n```\nvar publish = function() {\n  console.log(bodyValue()); \n};\n```\n",
-        tags: [
-            { color: "#5aba59", name: "JS" },
-            { color: "#4d85d1", name: "Solid" }
-        ]
-    },
-    "https://example.org/post2": {
-        url: "https://example.org/post2",
-        title: "Everything You Need to Know About Solid",
-        author: "https://deiu.me/profile#me",
-        date: "3 Dec 2015",
-        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        tags: [
-            { color: "#df2d4f", name: "Linked Data" },
-            { color: "#4d85d1", name: "Solid" }
-        ]
-    },
-    "https://example.org/post3": {
-        url: "https://example.org/post3",
-        title: "Photos from CSSConf and JSConf",
-        author: "https://deiu.me/profile#me",
-        date: "1 Dec 2015",
-        body: '<img src="https://deiu.me/Public/stata-background.png">',
-        tags: [
+// var posts = {
+//     "https://example.org/post1": {
+//         url: "https://example.org/post1",
+//         title: "Introducing Solid",
+//         author: "https://deiu.me/profile#me",
+//         date: "4 Dec 2015",
+//         body: "![test](https://deiu.me/avatar.jpg) \n\n```\nvar publish = function() {\n  console.log(bodyValue()); \n};\n```\n",
+//         tags: [
+//             { color: "#5aba59", name: "JS" },
+//             { color: "#4d85d1", name: "Solid" }
+//         ]
+//     },
+//     "https://example.org/post2": {
+//         url: "https://example.org/post2",
+//         title: "Everything You Need to Know About Solid",
+//         author: "https://deiu.me/profile#me",
+//         date: "3 Dec 2015",
+//         body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+//         tags: [
+//             { color: "#df2d4f", name: "Linked Data" },
+//             { color: "#4d85d1", name: "Solid" }
+//         ]
+//     },
+//     "https://example.org/post3": {
+//         url: "https://example.org/post3",
+//         title: "Photos from CSSConf and JSConf",
+//         author: "https://deiu.me/profile#me",
+//         date: "1 Dec 2015",
+//         body: '<img src="https://deiu.me/Public/stata-background.png">',
+//         tags: [
 
-        ]
-    },
-    "https://example.org/post4": {
-        url: "https://example.org/post4",
-        title: "Rdflib.js 0.9 Released",
-        author: "https://user/profile#me",
-        date: "24 Nov 2015",
-        body: "We are happy to announce the release of Rdflib.js 0.9! You can find it now on github, download it directly, or pull it in via npm. We’ve also updated the Solid spec with the latest documentation.",
-        tags: [
-            { color: "#8156a7", name: "Other" }
-        ]
-    }
-};
+//         ]
+//     },
+//     "https://example.org/post4": {
+//         url: "https://example.org/post4",
+//         title: "Rdflib.js 0.9 Released",
+//         author: "https://user/profile#me",
+//         date: "24 Nov 2015",
+//         body: "We are happy to announce the release of Rdflib.js 0.9! You can find it now on github, download it directly, or pull it in via npm. We’ve also updated the Solid spec with the latest documentation.",
+//         tags: [
+//             { color: "#8156a7", name: "Other" }
+//         ]
+//     }
+// };
 
-var authors = {
-    "https://deiu.me/profile#me": {
-        name: "Andrei Sambra",
-        picture: "https://deiu.me/avatar.jpg"
-    },
-    "https://user/profile#me": {}
-};
+// var authors = {
+//     "https://deiu.me/profile#me": {
+//         name: "Andrei Sambra",
+//         picture: "https://deiu.me/avatar.jpg"
+//     },
+//     "https://user/profile#me": {}
+// };
 
 var getAuthorByWebID = function(webid) {
-    var name = '';
+    var name = 'Unknown';
     var picture = 'favicon.png';
     if (webid && webid.length > 0) {
         var author = authors[webid];
@@ -485,6 +455,67 @@ var addPostToDom = function(post) {
 
 var sortTag = function(name) {
     console.log(name);
+};
+
+var init = function() {
+    document.querySelector('.blog-picture').src = user.picture;
+    document.querySelector('.blog-title').innerHTML = user.title;
+    document.querySelector('.blog-tagline').innerHTML = user.tagline;
+
+    if (queryVals['view'] && queryVals['view'].length > 0) {
+        var url = decodeURIComponent(queryVals['view']);
+        showViewer(url);
+    } else if (queryVals['edit'] && queryVals['edit'].length > 0) {
+        var url = decodeURIComponent(queryVals['edit']);
+        showEditor(url);
+    } else if (queryVals['new'] !== undefined) {
+        showEditor();
+    }
+    // select element holding all the posts
+    var postsdiv = document.querySelector('.posts');
+
+    // add all posts to viewer
+    if (posts && posts.length > 0) {
+        for (var i in posts) {
+            var article = addPostToDom(posts[i]);
+            postsdiv.appendChild(article);
+        }
+    } else {
+        // no posts, display a mock one
+        var acme = {
+            url: "https://example.org/post1",
+            title: "Welcome to Plume, a Solid blogging platform",
+            author: "https://example.org/user#me",
+            date: "3 Dec 2015",
+            body: "This is a demo post. Feel free to remove it whenever you wish.\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            tags: [
+                { color: "#df2d4f", name: "Decentralization" },
+                { color: "#4d85d1", name: "Solid" }
+            ]
+        };
+        posts[acme.url] = acme;
+        postsdiv.appendChild(addPostToDom(acme));
+    }
+
+    var header = document.querySelector('.header');
+    var header_height = getComputedStyle(header).height.split('px')[0];
+    var nav = document.querySelector('.nav');
+    var pic = document.querySelector('.blog-picture');
+    var pic_height = getComputedStyle(pic).height.split('px')[0];
+    var diff = header_height - pic_height;
+
+    function stickyScroll(e) {
+        if (window.pageYOffset > (diff + 50)) {
+            nav.classList.add('fixed-nav');
+        }
+
+        if(window.pageYOffset < (diff + 50)) {
+            nav.classList.remove('fixed-nav');
+        }
+    }
+
+    // Scroll handler to toggle classes.
+    window.addEventListener('scroll', stickyScroll, false);
 };
 
 // start app
