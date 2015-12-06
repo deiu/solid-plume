85974e955808c96f0c650f65feef8558448d43f9
deiu <andrei@fcns.eu>
2015-12-05 23:22:58 -0500

    Minor improvements

--- app/app.js
+++ app/app.js
@@ -257,19 +257,6 @@ var resetAll = function() {
 };
 
 var publishPost = function(url) {
-    function rgbToHex(color) {
-        color = color.replace(/\s/g,"");
-        var aRGB = color.match(/^rgb\((\d{1,3}[%]?),(\d{1,3}[%]?),(\d{1,3}[%]?)\)$/i);
-        if(aRGB)
-        {
-            color = '';
-            for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,'0$1');
-        }
-        else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, '$1$1$2$2$3$3');
-        return '#'+color;
-    };
-
-
     var post = {};
     post.url = (url && url.length>0)?url:user.webid+document.querySelector('.editor-title').innerHTML;
     post.title = document.querySelector('.editor-title').innerHTML;
--- app/util.js
+++ app/util.js
@@ -40,3 +40,16 @@ var notify = function(ntype, text) {
         note.remove();
     }, timeout);
 };
+
+// convert rgb(red, green, blue) to #hexa colors
+function rgbToHex(color) {
+    color = color.replace(/\s/g,"");
+    var aRGB = color.match(/^rgb\((\d{1,3}[%]?),(\d{1,3}[%]?),(\d{1,3}[%]?)\)$/i);
+    if(aRGB)
+    {
+        color = '';
+        for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,'0$1');
+    }
+    else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, '$1$1$2$2$3$3');
+    return '#'+color;
+};
