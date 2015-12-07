0e7636165bab1fa2beee8624bbf7b1baaa843dd9
deiu <andrei@fcns.eu>
2015-12-07 11:35:58 -0500

    Config file

---
+++ app/config.js
@@ -0,0 +1,12 @@
+
+var Plume = Plume || {};
+
+// Init some default config values
+Plume.config = {
+    owner: '',
+    title: "/dev/solid",
+    tagline: "Rocking the Solid Web",
+    picture: "img/logo-white.svg",
+    defaultPath: 'posts',
+    dataContainer: ''
+};
