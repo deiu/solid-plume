e4f49bcf940c3d6a9a83f3574a6de893667c819b
deiu <andrei@fcns.eu>
2015-12-08 09:28:14 -0500

    Sigh, forgot to remove test values

--- app/config.js
+++ app/config.js
@@ -3,11 +3,11 @@ var Plume = Plume || {};
 
 // Init some default config values
 Plume.config = {
-    owner: 'https://deiu.me/profile#me',
+    owner: '',
     title: "My Plume blog",
     tagline: "Light as a feather",
     picture: "img/logo.svg",
     fadeText: true,
     defaultPath: 'posts',
-    dataContainer: 'https://deiu.me/blog/posts/'
+    dataContainer: ''
 };
