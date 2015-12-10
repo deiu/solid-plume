2730396527fe533e709e5c0775b1cd5167b0c12c
deiu <andrei@fcns.eu>
2015-12-09 20:56:12 -0500

    Sigh, removed test URL

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ var Plume = Plume || {};
 
 // Init some default config values
 Plume.config = {
-    owner: 'https://deiu.me/blog/posts/',
+    owner: '',
     title: "Plume",
     tagline: "Light as a feather",
     picture: "img/logo.svg",
